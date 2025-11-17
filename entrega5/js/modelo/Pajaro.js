4b15cdb5e0b5a229ecd27770745af9bb599e2a41
Soledad <solcedro82@gmail.com>
2025-11-17 20:31:10 -0300

    cambios en Clase pajaro agrego métodos límites

--- entrega5/js/modelo/Pajaro.js
+++ entrega5/js/modelo/Pajaro.js
@@ -1,12 +1,13 @@
 export class Pajaro {
-    constructor(ruta) {
+    constructor(ruta, canvasHeight) {//por parametro paso la altura del canvas para los límites
         this.img = new Image();
         this.img.src = ruta;
 
         this.frameW = 100;
         this.frameH = 110;
         this.totalFrames = 4;
-        this.velAnim = 6;
+        this.velAnim = 8;
+        this.canvasHeight = canvasHeight;
 
         this.frameActual = 0;
         this.cont = 0;
@@ -37,6 +38,26 @@ export class Pajaro {
         this.velY = this.salto;
     }
 
+    fueraDelCanvas(){
+        const box= this.getColisionBox();
+
+        //colision con el techo
+        if(box.y < 0){
+            this.y=0;//matengo el pajaro en el límite del techo
+            return true;
+        }
+        //colision con el piso
+        if(box.y + box.h > this.canvasHeight){
+            //ajusto la posición para que quede en exactamente en el piso
+            //box.h es la altura de la caja de colisión, y 2 es el desplazamiento inicial de y
+            this.y= this.canvasHeight - box.h -2;
+            return true;
+        }
+
+        return false;
+    }
+
+    
     getColisionBox() {
         return {
             x: this.x + 2,
